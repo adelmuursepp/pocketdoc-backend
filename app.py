5eff3b41156c1775ce418dfaa736e05d896e6f95
Adel Müürsepp <adel.muursepp@gmail.com>
2024-03-16 14:30:54 -0400

    does not update summarys on admin page

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from dotenv import load_dotenv
-from flask import Flask, jsonify
+from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
 from utils.process_data import process_data
 from utils.get_database import get_database
+
 from routes.recommended_actions_route import recommended_actions_route
 from routes.admin_summary_route import admin_summary_route
 
 from flask_cors import CORS
-import sys
-
 
 load_dotenv()
 
@@ -24,22 +23,13 @@ scheduler = BackgroundScheduler(daemon=True)
 # Schedule the `check_google_sheets` function to run periodically
 scheduler.add_job(process_data, 'interval', seconds=6, max_instances=2) 
 
-dbname = get_database()
-print("Started the scheduler")
-sys.stdout.flush()
-
 @app.route('/')
 def home():
     return "Hello, Flask with MongoDB!"
 
-
-
 if __name__ == '__main__':
     dbname = get_database()
-    print("Started the scheduler")
-    sys.stdout.flush()
-    scheduler.start()
-    sys.stdout.flush()
+    scheduler.start()  # Start the scheduler
     try:
         app.run(use_reloader=False)  # Prevents the scheduler from being started twice in debug mode
     except (KeyboardInterrupt, SystemExit):
--- utils/process_data.py
+++ utils/process_data.py
@@ -3,7 +3,6 @@ from utils.get_database import get_database
 from utils.fetch_recommendation import fetch_recommendation
 from utils.fetch_urgency import fetch_urgency
 from utils.save_admin_summary import save_admin_summary
-import sys
 
 def process_data():
     res = read_google_sheet()
@@ -16,7 +15,6 @@ def process_data():
     transcript_collection = dbname["transcript"]
     db_transcripts = list(transcript_collection.find())
     if len(db_transcripts) == 0:
-        print('db transcripts is 0', db_trans)
         transcript_collection.insert_one({"time": time, "summary": summary})
     else:
         db_time = db_transcripts[0]["time"] # There should only be one transcript in array
@@ -34,8 +32,5 @@ def process_data():
             urgency = fetch_urgency(summary)
             save_admin_summary(name, time, urgency, summary)
             print("URGENCY UPDATED")
-
-            sys.stdout.flush()
         else:
             print("NO UPDATES")
-            sys.stdout.flush()
