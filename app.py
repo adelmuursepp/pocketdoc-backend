dbe1787c633b8baa8a13b108a151c1265e14d469
Adel Müürsepp <adel.muursepp@gmail.com>
2024-03-16 11:34:55 -0400

    Added summaries route

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from google_sheets import read_google_sheet
 from utils.process_data import process_data
 from utils.get_database import get_database
 from routes.recommended_actions_route import recommended_actions_route
+from routes.summaries_route import summaries_route
 from flask_cors import CORS
 import sys
 
@@ -17,6 +18,7 @@ CORS(app)
 app.app_context().push()
 
 app.register_blueprint(recommended_actions_route)
+app.register_blueprint(summaries_route)
 
 scheduler = BackgroundScheduler(daemon=True)
 
@@ -26,17 +28,21 @@ scheduler.add_job(process_data, 'interval', seconds=6, max_instances=2)
 dbname = get_database()
 print("Started the scheduler")
 sys.stdout.flush()
-scheduler.start()
 
 @app.route('/')
 def home():
     return "Hello, Flask with MongoDB!"
 
+
+
 if __name__ == '__main__':
     dbname = get_database()
     print("Started the scheduler")
     sys.stdout.flush()
-    scheduler.start()  # Start the scheduler
+    if scheduler.running:
+        scheduler.shutdown()
+    if not scheduler.running:
+        scheduler.start()
     sys.stdout.flush()
     try:
         app.run(use_reloader=False)  # Prevents the scheduler from being started twice in debug mode
